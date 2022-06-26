53e252f7a9fc65f32f92d86a010a1572fe588ea8
Htet04 <aabb36335@gmail.com>
2022-06-26 14:18:29 +0630

    repl up rm

--- app/src/main/java/com/jav/info/MainActivity.java
+++ app/src/main/java/com/jav/info/MainActivity.java
@@ -169,7 +169,7 @@ public class MainActivity extends AppCompatActivity {
 		public int getCount() {
 			return _data.size();
 		}
-		
+
 		@Override
 		public HashMap<String, Object> getItem(int _index) {
 			return _data.get(_index);
@@ -303,15 +303,14 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		//group id,item id,order ,title
-		//menu.add(0, 1, 1, "Scroll after add").setCheckable(true).setChecked(isScroll);
-		getMenuInflater().inflate(R.menu.main_menu,menu);
+		menu.add(0, 1, 1, "Scroll after add").setCheckable(true).setChecked(isScroll);
 		return super.onCreateOptionsMenu(menu);
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-		case (R.id.menu_canScroll): {
+		case (1): {
 			if (isScroll) {
 				isScroll = false;
 			} else {
@@ -424,6 +423,8 @@ public class MainActivity extends AppCompatActivity {
 	private void shm(Object s) {
 		if (s == null)
 			return;
-		Toast.makeText(ctx(), String.valueOf(s).toString(), Toast.LENGTH_SHORT).show();
+		{
+			Toast.makeText(ctx(), String.valueOf(s).toString(), Toast.LENGTH_SHORT).show();
+		}
 	}
 }
